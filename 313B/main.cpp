7b433186f732a4812bf7ba05694b45efa48c3f09
Jefferson <jeffersonhcarvalho@gmail.com>
2018-05-08 09:22:57 -0300

    Added 313B

--- 313B/main.cpp
+++ 313B/main.cpp
@@ -1,11 +1,22 @@
 # include <iostream>
+# include <string>
 
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    
+    string s;
+    int m, l, r, dp[100001];
+
+    cin >> s >> m;
+    dp[0] = 0;
+    for (size_t i = 1; i <= s.size(); ++i) dp[i] = dp[i - 1] + (s[i] == s[i - 1]);
+
+    while (m--) {
+        cin >> l >> r;
+        cout << dp[--r] - dp[--l] << '\n';
+    }
 
     return 0;
 }
