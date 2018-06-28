6f89d5f38bdd64954c9cbba755535ec27987bff5
Jefferson <jeffersonhcarvalho@gmail.com>
2018-06-27 22:28:48 -0300

    Fixed educational round 46 A

--- educational-round-46/A/main.cpp
+++ educational-round-46/A/main.cpp
@@ -1,47 +1,22 @@
-// Not finished yet
 # include <iostream>
-# include <set>
 # include <string>
 
 using namespace std;
 
-struct compare {
-    bool operator () (const string &lhs, const string &rhs) {
-        return lhs.size() < rhs.size();
-    }
-};
-
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n, count = 0, cur_count, word_count;
-    multiset<string, compare> a, b;
-    string cur;
+    int n, count = 0;
+    string a[105], b[105];
 
     cin >> n;
-    for (int i = 0; i < n; ++i) { cin >> cur; a.insert(cur); }
-    for (int i = 0; i < n; ++i) { cin >> cur; b.insert(cur); }
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < n; ++i) cin >> b[i];
 
-    auto to_erase = a.begin();
-    for (auto it1 = a.begin(); it1 != a.end(); ++it1) {
-        word_count = 1e9;
-        for (auto it2 = b.begin(); it2 != b.end(); ++it2) {
-            cur_count = 0;
-            if (it1->size() == it2->size()) {
-                for (int i = 0; i < it1->size(); ++i) {
-                    if (it1->at(i) != it2->at(i)) ++cur_count;
-                }
-                if (cur_count < word_count) {
-                    word_count = min(word_count, cur_count);
-                    to_erase = it2;
-                }
-            }
-        }
-        count += word_count;
-        a.erase(it1);
-        b.erase(it2);
-    }
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            if (a[i] == b[j]) { a[i] = b[j] = ""; ++count; break; }
 
-    cout << count << '\n';
+    cout << n - count << '\n';
 
     return 0;
 }
