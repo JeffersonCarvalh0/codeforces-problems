04ffe1679a6c91ddbbbec093f1681cc7de6783bf
Jefferson <jeffersonhcarvalho@gmail.com>
2018-12-05 09:40:09 -0300

    Added 221A

---
+++ 221A/main.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n;
+
+    cin >> n;
+
+    cout << n << ' ';
+    for (int i = 1; i <= n - 1; ++i) cout << i << ' ';
+    cout << '\n';
+
+    return 0;
+}
